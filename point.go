011f2f0c0c1b434ced357500dadd981ec4f1ff09
이혁 <dlgur911@gmail.com>
2022-07-24 18:47:47 +0900

    포인트 연습문제 정답

--- point.go
+++ point.go
@@ -7,9 +7,31 @@ type Data struct {
 	data  [200]int
 }
 
+type Actor struct {
+	Name  string
+	HP    int
+	Speed float64
+}
+
 func ChangeData(arg *Data) {
 	arg.value = 999
 	arg.data[100] = 999
+}
+
+func NewActor(name string, hp int, speed float64) *Actor {
+	/*
+		내가 한 것
+		var actor = &Actor{}
+		actor.Name = name
+		actor.HP = hp
+		actor.Speed = speed
+		return actor
+	*/
+	// return &Actor{name, hp, speed}
+	/*
+		var a = Actor{name, hp, speed}
+		return &a
+	*/
 }
 
 func main() {
@@ -26,4 +48,9 @@ func main() {
 	p.data[199] = 200
 	//ChangeData(p)
 	fmt.Printf("value: %d, data[100]: %d", data.value, data.data[199])
+	fmt.Println()
+
+	var actor = NewActor("금토끼", 99, 100)
+	fmt.Println(actor.Speed)
+	fmt.Println(actor.Name)
 }
